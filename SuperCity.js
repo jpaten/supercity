e425112c44c7c9b671d944323d3bb1e440e363c3
Jonah Paten <jpaten@ucla.edu>
2024-05-22 09:43:30 -0700

    First draft of tower (#3)

--- SuperCity.js
+++ SuperCity.js
@@ -1,8 +1,9 @@
 import {defs, tiny} from './examples/common.js';
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture
 } = tiny;
+
 
 export class SuperCity extends Scene {
     constructor() {
@@ -11,7 +12,9 @@ export class SuperCity extends Scene {
 
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
-            square: new defs.Square(3)
+            square: new defs.Square(3),
+            cylinder: new defs.Capped_Cylinder(50,50),
+            cube: new defs.Cube()
             // TODO:  Fill in as many additional shape instances as needed in this key/value table.
             //        (Requirement 1)
         };
@@ -20,6 +23,14 @@ export class SuperCity extends Scene {
         this.materials = {
             planet1: new Material(new defs.Phong_Shader(),
                 {color: hex_color("#969696"), ambient: 0, specularity: 0, diffusivity: 1}),
+            tower_blue: new Material(new defs.Phong_Shader(),
+                {color: hex_color("#5576a9"), ambient: 0.2, specularity: 0, diffusivity: 0.1}),
+            tower_window: new Material(new defs.Phong_Shader(),
+                {color: hex_color("#DDDDDD"), ambient: 1, specularity: 0.1, diffusivity: 0.4}),
+            helipad_texture: new Material(new defs.Textured_Phong(),
+                {color: hex_color("#000000"), texture: new Texture("./assets/helipad.png"), ambient: 1}),
+            door: new Material(new defs.Textured_Phong(),
+                {color: hex_color("#000000"), texture: new Texture("./assets/door.png"), ambient:1})
         }
 
         this.initial_camera_location = Mat4.look_at(vec3(0, -3, 5), vec3(0,0, 0), vec3(0, 1, 0));
@@ -39,6 +50,35 @@ export class SuperCity extends Scene {
     }
 
     display(context, program_state) {
+        const draw_tower = (context, program_state, x,y) => {
+            this.shapes.cylinder.draw(
+                context, program_state,
+                Mat4.identity().times(Mat4.translation(x,y,0)).times(Mat4.scale(1.2,1.2,8.5)),
+                this.materials.tower_blue
+            );
+            const window_positions = [-1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4];
+            for (let i = 0; i < window_positions.length; i++) {
+                this.shapes.cylinder.draw(
+                    context, program_state, Mat4.identity().times(Mat4.translation(x,y,window_positions[i])).times(Mat4.scale(1.201,1.201,0.2)),
+                    this.materials.tower_window
+                );
+            }
+            this.shapes.cylinder.draw(
+                context, program_state,
+                Mat4.identity().times(Mat4.translation(x,y,4.62)).times(Mat4.scale(1.2,1.2,0)),
+                this.materials.helipad_texture
+            )
+            this.shapes.cube.draw(
+                context, program_state,
+                Mat4.identity().times(Mat4.translation(x,y,-1.9)).times(Mat4.scale(1.5,1.5,1.5)),
+                this.materials.tower_blue
+            )
+            this.shapes.square.draw(
+                context, program_state,
+                Mat4.identity().times(Mat4.translation(x,y-1.51,-1.5)).times(Mat4.scale(0.5,0.5,0.5)).times(Mat4.rotation(Math.PI / 2.0, -1,0,0)),
+                this.materials.door
+            )
+        }
         // display():  Called once per frame of animation.
         // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
         if (!context.scratchpad.controls) {
@@ -66,11 +106,16 @@ export class SuperCity extends Scene {
             ))
         }
 
-        program_state.lights = [new Light(vec4(0, 0, 0, 1), color(1,1,1), 10000)];
+
+        program_state.lights = [new Light(vec4(-6, -6, 20, 1), color(1,1,1), 10000)];
 
         this.shapes.square.draw(context, program_state, Mat4.identity().times(Mat4.translation(0,0,-2)).times(Mat4.scale(20,20,20)), this.materials.planet1)
         this.shapes.square.draw(context, program_state, Mat4.identity().times(Mat4.translation(-2,-2,-1.9)).times(Mat4.scale(0.25,0.25,0.25)), this.materials.planet1.override({color:hex_color("#FF0000")}))
         this.shapes.square.draw(context, program_state, Mat4.identity().times(Mat4.translation(2,2,-1.9)).times(Mat4.scale(0.25,0.25,0.25)), this.materials.planet1.override({color:hex_color("#00FFFF")}))
+
+        draw_tower(context, program_state, 0,0)
+
+
         //this.shapes.square.draw(context, program_state, Mat4.identity().times(Mat4.translation(0,-1,-2)), this.materials.planet1)
         //this.shapes.square.draw(context, program_state, Mat4.identity().times(Mat4.translation(-2,-2,-2)), this.materials.planet1)
 
