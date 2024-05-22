b9ec8461b8afc6c4015884974434d69440864750
Jonah Paten <jpaten@ucla.edu>
2024-05-22 13:32:22 -0700

    Added movable tiles (#8)

--- SuperCity.js
+++ SuperCity.js
@@ -4,6 +4,9 @@ const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture
 } = tiny;
 
+const BASE_Z = -2
+
+
 export class SuperCity extends Scene {
     constructor() {
         // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
@@ -41,8 +44,9 @@ export class SuperCity extends Scene {
         }
 
         this.initial_camera_location = Mat4.look_at(vec3(0, -3, 5), vec3(0,0, 0), vec3(0, 1, 0));
-        this.selection = [0,1]
+        this.selection = [0,1];
     }
+
 
     make_control_panel() {
         // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
